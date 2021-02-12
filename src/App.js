12ebf162931f41170b3c1e021225c17d7f0927bc
solomon igbigbi <solomonigbigbi07@gmail.com>
2021-02-12 14:06:06 +0100

    finished with Sign In Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header-compnonents/header-components'
 import HomePage from './pages/homepage/hompage.component';
 import Shop from './pages/shoppage/shoppage.components'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out'
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
@@ -12,8 +13,9 @@ function App() {
     <div> 
       <Header />
       <Switch>
-        <Route exact='true' path='/' component={ HomePage } />
-        <Route exact='true' path='/shop' component={ Shop } />   
+        <Route exact path='/auth' component={ SignInAndSignUpPage } />
+        <Route exact path='/' component={ HomePage } />
+        <Route exact path='/shop' component={ Shop } />   
       </Switch>
     </div>
   ) 
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './sign-in.style.scss'
+
+import FormInput from '../../components/form-input/form-input.component'
+import CustomButton from '../../components/custom-button/custom-button.component'
+
+class SignIn extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ email: '', password: ''});
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value})
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>sign in with email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email" name='email' label='Email' handleChange={this.handleChange} value={this.state.email} />
+                    <FormInput type="password"  name='password' label='Password' handleChange={this.handleChange} required value={this.state.password} />
+
+                    <CustomButton type="submit"> SIGN IN </CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
